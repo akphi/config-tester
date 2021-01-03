b9b65a4316b6c0110ad4dae400b8cf7cc2aef4fa
An Phi <aphi@skidmore.edu>
2021-01-03 17:51:18 -0500

    feat(lib1): update helloworld program

--- packages/lib1/src/foo.ts
+++ packages/lib1/src/foo.ts
@@ -9,7 +9,6 @@ import type { A } from './something';
 import { Am } from './something';
 
 export function helloworld() {
-  const b: A = { car: '1' };
-  const a = Am; // asdasdasdasdasdasdasdasdasd asdasdasdasdasdasdasdasdasdasdasdasd  asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd
+  console.log('helloworld!');
   return 'Hello world';
 }
