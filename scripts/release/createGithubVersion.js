5d3609f247294e81efd5ec163c30d0f5938772e7
An Phi <aphi@skidmore.edu>
2021-09-04 07:36:11 -0400

    dummy

---
+++ scripts/release/createGithubVersion.js
@@ -0,0 +1,86 @@
+const github = require('@actions/github');
+const { execSync } = require('child_process');
+const chalk = require('chalk');
+
+const postChangesetPublishCleanup = async () => {
+  const publishedPackages = JSON.parse(process.env.PUBLISHED_PACKAGES);
+  const tagsToCleanup = publishedPackages.map(
+    (pkg) => `${pkg.name}@${pkg.version}`,
+  );
+  const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+  const mainPackageName = process.env.MAIN_PACKAGE;
+  const releaseVersion = publishedPackages.find(
+    (pkg) => pkg.name === mainPackageName,
+  );
+
+  console.log(
+    `Removing Github releases and tags created by changesets/action...`,
+  );
+  const tagsNotRemoved = [];
+  await Promise.all(
+    tagsToCleanup.map(async (tag) => {
+      try {
+        // Delete the release published on Github by `changesets/action`
+        // Check the following links for Github actions API reference
+        // See https://octokit.github.io/rest.js/v18/
+        // See https://docs.github.com/en/rest/reference/repos#releases
+        const release = await octokit.rest.repos.getReleaseByTag({
+          tag,
+          ...github.context.repo,
+        });
+        await octokit.rest.repos.deleteRelease({
+          release_id: release.data.id,
+          ...github.context.repo,
+        });
+        // Delete the tags published by `changesets/action`
+        execSync(`git push --delete origin ${tag}`, {
+          cwd: process.cwd(),
+          stdio: ['pipe', 'pipe', 'inherit'], // only print error
+        });
+        console.log(chalk.green(`\u2713 Removed release and tag ${tag}`));
+      } catch (error) {
+        tagsNotRemoved.push(tag);
+        console.log(
+          chalk.green(
+            `\u2A2F Can't remove release and tag ${tag}. Error:\n${error.message}`,
+          ),
+        );
+        console.log(
+          `Can't remove release and tag ${tag}. Error:\n${error.message}`,
+        );
+      }
+    }),
+  );
+
+  if (tagsNotRemoved.length) {
+    console.warn(
+      `The following tags and their respective releases are not removed from Github. Please manually remove them on Github:\n${tagsNotRemoved
+        .map((tag) => `- ${tag}`)
+        .join('\n')}`,
+    );
+  }
+
+  if (releaseVersion) {
+    try {
+      await octokit.rest.repos.createRelease({
+        tag_name: `v${releaseVersion}`,
+        name: 'Untitled',
+        draft: true,
+        ...github.context.repo,
+      });
+      console.log(
+        chalk.green(
+          `\u2713 Successfully created release for tag v${releaseVersion}. Note that this is a draft release only, please fill in the details and publish the release on Github.`,
+        ),
+      );
+    } catch (error) {
+      console.log(
+        chalk.green(
+          `\u2713 Failed to create release with tag v${releaseVersion}. Please manually create this release tag on Github.`,
+        ),
+      );
+    }
+  }
+};
+
+postChangesetPublishCleanup();
