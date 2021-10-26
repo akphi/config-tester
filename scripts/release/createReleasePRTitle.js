26902cec4d28fec52424ec6a68f679a02b40594f
An Phi <aphi@skidmore.edu>
2021-10-25 21:14:34 -0400

    more test

--- scripts/release/createReleasePRTitle.js
+++ scripts/release/createReleasePRTitle.js
@@ -1,3 +1,8 @@
 import * as githubActionCore from '@actions/core';
 
 githubActionCore.warning('Aiyoo');
+githubActionCore.warning('Achoo');
+githubActionCore.warning('Dot');
+githubActionCore.warning('Okl');
+githubActionCore.warning('Huh?');
+githubActionCore.error('Oops?');
